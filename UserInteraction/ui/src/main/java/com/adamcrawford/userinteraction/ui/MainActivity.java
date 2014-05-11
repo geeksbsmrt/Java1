c4834b9a70ccfad103915114a06741029798a4f2
Adam crawford <geeksbsmrt@gmail.com>
2014-05-10 20:00:53 -0400

    Sync Week 2 - User Interface

---
+++ UserInteraction/ui/src/main/java/com/adamcrawford/userinteraction/ui/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+* Crated by Adam Crawford
+* Full Sail Online MDVBS
+* Java 1 Week 2 - User Interaction
+ */
+
+
+package com.adamcrawford.userinteraction.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}

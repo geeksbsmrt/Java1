8ea61e1189b71c12f215b56f818214833b7d3484
Adam crawford <geeksbsmrt@gmail.com>
2014-05-12 22:41:22 -0400

    Project rewrite.  Undo previous commit leaving core alone.

--- UserInteraction/ui/src/main/java/com/adamcrawford/userinteraction/ui/MainActivity.java
+++ UserInteraction/ui/src/main/java/com/adamcrawford/userinteraction/ui/MainActivity.java
@@ -7,12 +7,8 @@ package com.adamcrawford.userinteraction.ui;
  */
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import com.adamcrawford.userinteraction.ui.data.JsonHandler;
 
-import org.json.JSONObject;
 
 public class MainActivity extends Activity {
 
@@ -22,35 +18,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         final JsonHandler jsonHandler = new JsonHandler();
-        Button subBtn = (Button) findViewById(R.id.submitButton);
-        final Button dispBtn = (Button) findViewById(R.id.dispButton);
 
-        View.OnClickListener clicked = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.submitButton : {
-                        JSONObject userData = new JSONObject();
-                        dispBtn.setVisibility(View.VISIBLE);
-                        jsonHandler.setJSON(userData);
-                        break;
-                    }
-                    case R.id.dispButton : {
-                        System.out.println("Get pressed");
-                        view.setVisibility(View.GONE);
-                        break;
-                    }
-                    default:
-                        break;
-                }
-            }
-        };
-
-        subBtn.setOnClickListener(clicked);
-        dispBtn.setOnClickListener(clicked);
-
-
-        //jsonHandler.getJSON();
+        jsonHandler.getJSON();
 
     }
 

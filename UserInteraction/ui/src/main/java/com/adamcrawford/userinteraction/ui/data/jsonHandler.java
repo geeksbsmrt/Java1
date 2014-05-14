77b8ce1095f9320820a62c250dd72c850231b777
Adam crawford <geeksbsmrt@gmail.com>
2014-05-13 23:07:57 -0400

    Comments for how to structure app.  Building across multiple machines, using Git to sync.

---
+++ UserInteraction/ui/src/main/java/com/adamcrawford/userinteraction/ui/data/jsonHandler.java
@@ -0,0 +1,26 @@
+package com.adamcrawford.userinteraction.ui.data;
+
+/**
+ * Created by Adam Crawford
+ * Full Sail Online MDVBS
+ * Java 1 Week 2 - User Interaction
+ */
+
+import org.json.JSONObject;
+
+public class JsonHandler {
+//TODO Implement based on WoW Guild Member API return
+    public JsonHandler() {
+    }
+
+    public String getJSON () {
+        String placeHolder = new String();
+        System.out.println("in getJson");
+        return placeHolder;
+    }
+
+    public void setJSON (JSONObject json) {
+        System.out.println("setting JSON");
+    }
+
+}

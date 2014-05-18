fa7b3f93d65192dac0584250c33d0ccd24787e81
Adam Crawford <geeksbsmrt@gmail.com>
2014-05-18 19:57:33 -0400

    Initial Week 3 Commit.  Will be rebuilding based on UserInteraction due to different JSON Parsing and visual layout needed.

---
+++ DataManagement/dm/src/main/java/com/adamcrawford/datamanagement/dm/MainActivity.java
@@ -0,0 +1,22 @@
+package com.adamcrawford.datamanagement.dm;
+
+/**
+ * Created by Adam Crawford on May 18, 2014.
+ * Full Sail University Online MDVBS
+ * Java 1 Week 3 - Data Management
+ */
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}

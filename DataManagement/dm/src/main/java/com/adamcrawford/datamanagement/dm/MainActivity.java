7bf56b7aa181fcc960ef05a8bcbf094db469fbef
Adam Crawford <geeksbsmrt@gmail.com>
2014-05-19 12:12:03 -0400

    fix JSONHandler

--- DataManagement/dm/src/main/java/com/adamcrawford/datamanagement/dm/MainActivity.java
+++ DataManagement/dm/src/main/java/com/adamcrawford/datamanagement/dm/MainActivity.java
@@ -8,15 +8,44 @@ package com.adamcrawford.datamanagement.dm;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class MainActivity extends Activity {
+
+private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+	    Log.wtf(TAG, "test msg");
+
+	    Boolean isConnected = getStatus(this);
+
+	    if (isConnected) {
+		    Log.i(TAG, "You are connected");
+	    } else {
+		    Log.i(TAG, "You are not connected");
+	    }
+
     }
 
+	public Boolean getStatus(Context c)
+	{
+
+		ConnectivityManager conMan = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = conMan.getActiveNetworkInfo();
+
+		if (netInfo != null) {
+			return netInfo.isConnected();
+		} else {
+			return false;
+		}
+	}
 }
--- UserInteraction/ui/src/main/java/com/adamcrawford/userinteraction/ui/data/jsonHandler.java
+++ UserInteraction/ui/src/main/java/com/adamcrawford/userinteraction/ui/data/jsonHandler.java
@@ -6,21 +6,72 @@ package com.adamcrawford.userinteraction.ui.data;
  * Java 1 Week 2 - User Interaction
  */
 
+import com.adamcrawford.userinteraction.ui.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonHandler {
-//TODO Implement based on WoW Guild Member API return
+
     public JsonHandler() {
     }
+	// get JSON data from chosen option
+    public static JSONObject getJSON (String chosen) {
+		// vars for manipulation
+        String guild, name, faction, players;
+	    JSONArray characters;
 
-    public String getJSON () {
-        String placeHolder = new String();
-        System.out.println("in getJson");
-        return placeHolder;
+	    //build JSON data
+	    JSONObject jObject = setJSON();
+
+	    try {
+		    //get info from JSON
+		    name = jObject.getJSONObject("data").getJSONObject(chosen).getString("name");
+		    faction = jObject.getJSONObject("data").getJSONObject(chosen).getString("faction");
+		    players = jObject.getJSONObject("data").getJSONObject(chosen).getString("players");
+		    characters = jObject.getJSONObject("data").getJSONObject(chosen).getJSONArray("characters");
+
+		    System.out.println(characters.length());
+
+		    for (int i=0,j=characters.length(); i<j; i++) {
+			    System.out.println(characters.getString(i));
+		    }
+
+		    guild = String.format("%s %s \r\n %s %s \r\n %s %s \r\n %s", R.string.gText, name, R.string.faction, faction, R.string.numPlayers, players, characters.toString());
+	    } catch (JSONException e) {
+		    e.printStackTrace();
+		    guild = String.format("%s", R.string.guildError);
+	    }
+		System.out.println(guild);
+        return jObject;
     }
 
-    public void setJSON (JSONObject json) {
-        System.out.println("setting JSON");
+    public static JSONObject setJSON () {
+	    System.out.println("setting JSON");
+
+	    JSONObject root = new JSONObject();
+	    try {
+		    JSONObject data = new JSONObject();
+			//build json from dataconst.
+		    for (DataConstructor.Guilds guild : DataConstructor.Guilds.values()) {
+			    JSONObject myGuild = new JSONObject();
+			    myGuild.put("name", guild.getGuildName());
+			    myGuild.put("faction", guild.getGuildFaction());
+			    myGuild.put("players", guild.getPlayers());
+
+			    myGuild.put("characters", guild.getCharacters());
+
+			    data.put(guild.name(), myGuild);
+
+		    }
+
+		    root.put("data", data);
+	    } catch (JSONException e) {
+		    e.printStackTrace();
+	    }
+
+	    return root;
     }
 
 }
